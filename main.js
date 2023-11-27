849a7bd219d5b077a841522c330e8b6a5171e1c8
Serhii Didenko <srg.dd0@gmail.com>
2023-11-27 11:42:21 +0200

    fix#4

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 
+// Middleware to log information about each incoming request
+app.use((req, res, next) => {
+  console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
+  next();
+});
+
+// Route for the root path
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+// Dynamic route for personalized greeting
+app.get('/greet/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello, ${name}! Welcome to the Express.js app.`);
 });
 
 const PORT = process.env.PORT || 3000;
